216914cefb025577a1592054dfc55d699651f675
Greg Brown <gregbrowndev@gmail.com>
2021-11-22 08:57:56 +0000

    refactor graphql into modules

--- growth/adapters/inbound/graphql/main.py
+++ growth/adapters/inbound/graphql/main.py
@@ -3,12 +3,11 @@ import pathlib
 from ariadne import load_schema_from_path, make_executable_schema
 from ariadne.asgi import GraphQL
 
-from growth.adapters.inbound.graphql.mutations import mutation
-from growth.adapters.inbound.graphql.queries import query, tariff
+from growth.adapters.inbound.graphql.mutations.types import mutation
+from growth.adapters.inbound.graphql.queries.types import query, tariff
 
 DIR = pathlib.Path(__file__).resolve().parent
-SCHEMA_LOCATION = str(DIR.joinpath("schema.graphql"))
+type_defs = load_schema_from_path(str(DIR))
 
-type_defs = load_schema_from_path(SCHEMA_LOCATION)
 schema = make_executable_schema(type_defs, query, tariff, mutation)
 app = GraphQL(schema, debug=True)
